2919beaf460dcc8bbb4a69c8606e8055f548c1b5
tenhold <tenhold@gmail.com>
2020-09-22 11:14:58 -0500

    wroking on api

--- database/routes/drinks.js
+++ database/routes/drinks.js
@@ -1,11 +1,34 @@
 const router = require('express').Router();
 const Drink = require('../models/drinks.model');
+const { drinks } = require('../../data.json');
 
 // get 
-router.Route('/').get((req, res) => {
-  // 
-});
+// router.Route('/').get((req, res) => {
+//   // 
+// });
 
 // post
 
 
+
+
+
+
+
+
+////////////////////        mock up of return array           //////////////////////////////
+
+const drinkName = drinks.map(drink => {
+  const { strDrink,  strInstructions, strDrinkThumb, strIngredient1, strIngredient2 } = drink;
+  return { 
+    drink: strDrink,  
+    liquor: [strIngredient1, strIngredient2],
+    image: strDrinkThumb, 
+    instructions: strInstructions, 
+  };
+});
+
+console.log(drinkName);
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
