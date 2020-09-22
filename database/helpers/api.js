0819557d85a7ad2dca30a0db7bd1a7a5cdcebdbf
tenhold <tenhold@gmail.com>
2020-09-22 00:46:36 -0500

    working on api

--- database/helpers/api.js
+++ database/helpers/api.js
@@ -1,4 +1,14 @@
 const axios = require('axios');
 const config = require('config');
 
-console.log('hello')
+const getCocktails = (drink) => {
+  /*
+  pass in the info from the api to get the drink
+  */
+  const options = {
+    method: 'get',
+    url: 'https://the-cocktail-db.p.rapidapi.com/popular.php';
+  }
+};
+
+module.exports = getCocktails;
